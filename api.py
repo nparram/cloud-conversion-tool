a4afd42bda36977b75631aaf44511a38ca854ba2
Nilson Parra <np.martinez@mensajerosurbanos.com>
2021-12-01 22:47:11 -0500

    cambio en upload path

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ db = SQLAlchemy(app, engine_options={"pool_size": 6})
 ma = Marshmallow(app)
 app.config["JWT_SECRET_KEY"] = "cloud-coversor-jwt"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=6)
-app.config['UPLOAD_PATH'] = '/files'
+app.config['UPLOAD_PATH'] = './'
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024
 UPLOAD_FOLDER = "uploads"
 BUCKET = str(os.environ.get('BUCKET_NAME'))
